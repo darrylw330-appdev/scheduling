14a60637344c6e861e28d36cc82302f86f203d21
Darryl Williams <126805312+darrylw330@users.noreply.github.com>
2023-07-28 15:56:39 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # config/routes.rb
 Rails.application.routes.draw do
   resources :users
+  resources :work_days
+  resources :work_times
+  resources :bookings
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
