a8fca5a2e3ce0bdbbed1de8f860c5cd53ce6e7a2
Darryl Williams <126805312+darrylw330@users.noreply.github.com>
2023-07-31 17:43:26 +0000

    deleted work_day, work_time and bookings migrations due to error

--- db/migrate/20230728154908_create_work_times.rb
+++
@@ -1,9 +0,0 @@
-class CreateWorkTimes < ActiveRecord::Migration[7.0]
-  def change
-      create_table :work_times do |t|
-      t.datetime :start_time
-      t.datetime :end_time
-      t.timestamps
-    end
-  end
-end
