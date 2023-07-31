b53c9f9ce2de8a052a9bc6d8512ccc4c09d39abd
Darryl Williams <126805312+darrylw330@users.noreply.github.com>
2023-07-31 18:05:20 +0000

    deleted migrations again, idk at this point

--- db/migrate/20230731174439_create_work_times.rb
+++
@@ -1,9 +0,0 @@
-class CreateWorkTimes < ActiveRecord::Migration[7.0]
-  def change
-    create_table :work_times do |t|
-      t.time :start_time
-      t.time :end_time
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20230731174851_drop_work_days_table.rb
+++
@@ -1,5 +0,0 @@
-class DropWorkDaysTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :work_days, if_exists: true
-  end
-end
