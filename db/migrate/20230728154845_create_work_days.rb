a320d110dcac170de7c5b4612d87d7f9067f44f4
Darryl Williams <126805312+darrylw330@users.noreply.github.com>
2023-07-31 17:33:46 +0000

    updated create_work_days migration file

--- db/migrate/20230728154845_create_work_days.rb
+++ db/migrate/20230728154845_create_work_days.rb
@@ -1,8 +1,12 @@
 class CreateWorkDays < ActiveRecord::Migration[7.0]
   def change
-      create_table :work_days do |t|
+    create_table :work_days do |t|
       t.string :day_name
       t.timestamps
+    end
+
+    # Add a condition to create the index if it doesn't exist
+    unless index_exists?(:work_days, :day_name)
       add_index :work_days, :day_name
     end
   end
